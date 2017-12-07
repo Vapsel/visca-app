a721880a6801ec929be9ccddf601422844e26401
Vadym Perepeliak <vapsel@gmail.com>
2017-12-07 09:16:47 +0100

    Prepare UI and js

--- src/main/java/pl/edu/agh/kis/visca/controllers/view/MainPageViewController.java
+++ src/main/java/pl/edu/agh/kis/visca/controllers/view/MainPageViewController.java
@@ -1,15 +1,30 @@
 package pl.edu.agh.kis.visca.controllers.view;
 
+import jssc.SerialPortException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import pl.edu.agh.kis.visca.service.ViscaCommandSender;
+import pl.edu.agh.kis.visca.service.ViscaResponseReader;
 
 @Controller
 @RequestMapping("/")
 public class MainPageViewController {
 
+    private final ViscaCommandSender viscaCommandSender;
+
+    @Autowired
+    public MainPageViewController(ViscaCommandSender viscaCommandSender) {
+        this.viscaCommandSender = viscaCommandSender;
+    }
+
     @RequestMapping(method = RequestMethod.GET)
-    private String loadMainPage(){
+    private String loadMainPage(Model model) throws ViscaResponseReader.TimeoutException, SerialPortException {
+//        Integer maxSpeed = viscaCommandSender.sendGetPanTiltMaxSpeed();
+        Integer maxSpeed = 10;
+        model.addAttribute("maxSpeed", maxSpeed);
         return "mainPage";
     }
 }
--- src/main/resources/static/resources/js/mainPage.js
+++ src/main/resources/static/resources/js/mainPage.js
@@ -1,17 +1,57 @@
 $(function () {
+
     $('.visca-position-up').on('click', function () {
         var data = {
-            'direction': 'UP'
+            'direction': 'UP',
+            'speed': $('input').eq(0).val()
         };
        postAPI('/position', data);
     });
 
     $('.visca-position-down').on('click', function () {
         var data = {
-            'direction': 'DOWN'
+            'direction': 'DOWN',
+            'speed': $('input').eq(0).val()
         };
         postAPI('/position', data);
     });
+    $('.visca-position-left').on('click', function () {
+        var data = {
+            'direction': 'LEFT',
+            'speed': $('input').eq(0).val()
+        };
+        postAPI('/position', data);
+    });
+    $('.visca-position-right').on('click', function () {
+        var data = {
+            'direction': 'RIGHT',
+            'speed': $('input').eq(0).val()
+        };
+        postAPI('/position', data);
+    });
+    // ----------------------------------- Zoom
+    $('.visca-zoom-tele').on('click', function () {
+        var data = {
+            'zoom': 'TELE',
+            'speed': $('input').eq(0).val()
+        };
+        postAPI('/zoom', data);
+    });
+    $('.visca-zoom-wide').on('click', function () {
+        var data = {
+            'zoom': 'WIDE',
+            'speed': $('input').eq(0).val()
+        };
+        postAPI('/zoom', data);
+    });
+    $('.visca-other-home').on('click', function () {
+        var data = {
+            'command': 'HOME',
+            'speed': $('input').eq(0).val()
+        };
+        postAPI('/other', data);
+    });
+
 
 });
 
@@ -21,7 +61,7 @@ function postAPI(endpoint, data) {
         url: '/api' + endpoint,
         data: data,
         success: function(msg){
-            console.log('Response: ' + msg);
+            $('.response').val(msg);
         }
     });
 }
